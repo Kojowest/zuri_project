a028f54441e1277b4e72ed4204fb9c2b23427c4b
Kojowest <kojowest2012@gmail.com>
2022-07-25 02:57:32 +0000

    initial authentication system more can be added

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Home Page</h2>
+        <p>Everyone has access without authentication</p>
+	</div>
+	
+    
+</body>
+</html>
